81559ed47aec15c3e6162588de5aed4afb493f1f
Mariah <mariahmaic23@gmail.com>
2022-10-17 00:43:13 -0500

    Added Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # root 'home#home'
-  get "/", to: "home#index"
+  root to: "home#index"
 
   get "about", to: "about#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
